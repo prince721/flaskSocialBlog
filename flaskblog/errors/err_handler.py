fdd3d9c0229b1db795a6ab12cd4ed74115fec995
Prince Mishra <mishraprince721@gmail.com>
2020-05-16 12:06:22 +0530

    First Commit of Flask Social Blog Project

---
+++ flaskblog/errors/err_handler.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template
+
+errors = Blueprint('errors', __name__)
+
+
+@errors.app_errorhandler(404)
+def error404(error):
+    return render_template('errors/404.html'), 404
+
+
+@errors.app_errorhandler(403)
+def error403(error):
+    return render_template('errors/403.html'), 403
+
+
+@errors.app_errorhandler(500)
+def error500(error):
+    return render_template('errors/500.html'), 500
+
+
+@errors.app_errorhandler(405)
+def error405(error):
+    return render_template('errors/405.html'), 405
